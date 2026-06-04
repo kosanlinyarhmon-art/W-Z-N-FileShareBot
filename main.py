5a01585dfe978f18d37b60ed418b6ff810426b3e
kosanlinyarhmon-art <kosanlinyarhmon@gmail.com>
2026-06-04 12:32:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,35 @@
 import os
-import sys
-import asyncio 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+import asyncio
 from flask import Flask
 from threading import Thread
-from pyrogram import Client, filters
-print("--- SCRIPT STARTED ---")
-sys.stdout.flush()
-from flask import Flask
-from threading import Thread
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, idle
 
+# Flask Setup
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Bot is running!"
 
-def run():
+def run_flask():
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
+# Bot Setup
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
 
-Bot = Client(
-    "File Store Bot",
-    bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH
-)
+Bot = Client("my_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-@Bot.on_message(filters.private & (filters.photo | filters.video | filters.voice | filters.document | filters.animation | filters.audio | filters.sticker))
-async def hagadmansa(bot, message):
-    msg = await message.reply("`Processing...`")
-    media = message.photo or message.video or message.voice or message.document or message.animation or message.audio or message.sticker 
-    link = f"https://t.me/{BOT_USERNAME}?start={media.file_id}"
-    share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now,%20Join%20@Hagadmansa"
-    
-    await msg.edit(
-        text=f"Here is your link: {link}",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Share now', url=share)]])
-    )
+async def main():
+    await Bot.start()
+    print("Bot is started!")
+    await idle() # ဒီကောင်က Bot ကို အသက်ဝင်နေအောင် ထိန်းထားပေးမှာပါ
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
-    keep_alive() 
+    # Flask ကို Thread နဲ့ အရင် run
+    Thread(target=run_flask).start()
     
-    print("Bot is starting...")
-    Bot.run()
+    # Bot ကို asyncio နဲ့ run
+    asyncio.run(main())
