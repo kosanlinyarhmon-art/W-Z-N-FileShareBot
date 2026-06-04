4cf3bb3c701302d5620d7767ab3d40bdb645ad1c
kosanlinyarhmon-art <kosanlinyarhmon@gmail.com>
2026-06-04 14:16:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 import os
-import sys
 import asyncio
-from flask import Flask
-from threading import Thread
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# ပြီးရင် Code ရဲ့ အောက်ဆုံးမှာ ဒီလိုလေး ပြင်ပါ:
-if __name__ == "__main__":
-    try:
-        Thread(target=run_flask).start()
-        print("Bot is starting...")
-        Bot.run()
-    except Exception as e:
-        print(f"--- CRITICAL ERROR: {e} ---")
-
-# Flask Setup
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run_flask():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
 # Bot Configuration
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
@@ -33,7 +8,7 @@ API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 
-# Proxy setup
+# Proxy setup (အကယ်၍ ဒီတစ်ခုနဲ့မရရင် နောက်တစ်ခုပြောင်းပါ)
 proxy = dict(hostname="118.107.29.235", port=1080)
 
 Bot = Client(
@@ -44,19 +19,14 @@ Bot = Client(
     proxy=proxy
 )
 
-@Bot.on_message(filters.private & (filters.photo | filters.video | filters.voice | filters.document | filters.animation | filters.audio | filters.sticker))
+@Bot.on_message(filters.private & filters.media)
 async def hagadmansa(bot, message):
     msg = await message.reply("`Processing...`")
-    media = message.photo or message.video or message.voice or message.document or message.animation or message.audio or message.sticker 
+    # media file_id ကိုယူခြင်း
+    media = message.photo or message.video or message.document
     link = f"https://t.me/{BOT_USERNAME}?start={media.file_id}"
-    share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now"
-    await msg.edit(
-        text=f"Here is your link: {link}",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Share now', url=share)]])
-    )
+    await msg.edit(f"Here is your link: {link}")
 
 if __name__ == "__main__":
-    # Flask ကို Background မှာ run
-    Thread(target=run_flask).start()
-    # Bot ကို run
+    print("Bot is starting...")
     Bot.run()
